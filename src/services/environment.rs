63a6a386aaddc0b5f6e02c49a1eb258dd62586e8
infiniwave <felixli101@outlook.com>
2022-06-09 13:57:38 -0500

    fix: use 0.0.0.0 instead

--- src/services/environment.rs
+++ src/services/environment.rs
@@ -8,7 +8,7 @@ lazy_static! {
         env::var("MONGODB_DATABASE").expect("MONGODB_DATABASE must be set");
     pub static ref JWT_SECRET: String = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
     pub static ref LISTEN_ADDRESS: String =
-        env::var("LISTEN_ADDRESS").unwrap_or_else(|_| "127.0.0.1:9000".to_string());
+        env::var("LISTEN_ADDRESS").unwrap_or_else(|_| "0.0.0.0:9000".to_string());
     pub static ref PUBLIC_ADDRESS: Ipv4Addr = env::var("PUBLIC_ADDRESS")
         .expect("PUBLIC_ADDRESS must be set")
         .parse::<Ipv4Addr>()
