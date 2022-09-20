078111e7efc93b7109edd28ed6e05578e46bcda3
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-09-20 23:25:22 +0000

    fix: empty and long messages

--- src/methods/messages.rs
+++ src/methods/messages.rs
@@ -5,7 +5,7 @@ use dashmap::DashMap;
 
 use crate::services::socket::VoiceClient;
 
-use super::{GetChannelMessagesMethod, GetChannelMessagesResponse, SendChannelMessageMethod, SendChannelMessageResponse, Response};
+use super::{GetChannelMessagesMethod, GetChannelMessagesResponse, SendChannelMessageMethod, SendChannelMessageResponse, Response, ErrorResponse};
 
 pub async fn get_channel_messages(m: GetChannelMessagesMethod) -> Response {
     let messages = crate::services::database::messages::get_messages(m.channel_id).await;
@@ -17,8 +17,19 @@ pub async fn send_channel_message(
     clients: Arc<Mutex<DashMap<String, VoiceClient>>>,
     id: String,
 ) -> Response {
+    let trimmed = m.content.trim();
+    if trimmed.len() > 4096 {
+        return Response::Error(ErrorResponse {
+            error: "Message too long".to_string(),
+        });
+    }
+    if trimmed.len() < 1 {
+        return Response::Error(ErrorResponse {
+            error: "Message cannot be empty".to_string(),
+        });
+    }
     let clients_locked = clients.lock().await;
     let client = clients_locked.get(&id).unwrap();
-    let message = crate::services::database::messages::create_message(m.channel_id, client.get_user_id(), m.content).await;
+    let message = crate::services::database::messages::create_message(m.channel_id, client.get_user_id(), trimmed.to_owned()).await;
     Response::SendChannelMessage(SendChannelMessageResponse { message_id: message.id })
 }
