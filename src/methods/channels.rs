188dad1359ce5ff02053fc28e074b0735808bc42
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-01 14:44:49 -0500

    feat: channel methods

---
+++ src/methods/channels.rs
@@ -0,0 +1,151 @@
+use async_trait::async_trait;
+use dashmap::DashMap;
+use serde::{Serialize, Deserialize};
+
+use crate::{services::{socket::RpcClient, database::{channels::{get_channel, Channel, in_channel}, spaces::in_space}}, errors::Error};
+
+use super::{Respond, Response, ErrorResponse};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetChannelMethod {
+    channel_id: String,
+    // TODO: scopes
+    scope_id: Option<String>,
+    space_id: Option<String>,
+}
+
+#[async_trait]
+impl Respond for GetChannelMethod {
+    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Response {
+        let channel = get_channel(self.channel_id.clone()).await;
+        match channel {
+            Ok(channel) => {
+                let client = clients.get(&id).unwrap();
+                match channel {
+                    Channel::PrivateChannel { ref id, .. } | Channel::GroupChannel { ref id, .. } => {
+                        if self.space_id.is_some() {
+                            return Response::Error(ErrorResponse {
+                                error: Error::NotFound,
+                            });
+                        }
+                        let user_in_channel = in_channel(client.get_user_id(), id.clone()).await;
+                        match user_in_channel {
+                            Ok(user_in_channel) => {
+                                if !user_in_channel {
+                                    return Response::Error(ErrorResponse {
+                                        error: Error::NotFound,
+                                    });
+                                }
+                                Response::GetChannel(GetChannelResponse { channel })
+                            },
+                            Err(error) => Response::Error(ErrorResponse { error })
+                        }
+                    },
+                    Channel::InformationChannel { ref space_id, .. } | 
+                    Channel::AnnouncementChannel { ref space_id, .. } | 
+                    Channel::ChatChannel { ref space_id, .. } => {
+                        if let Some(request_space_id) = &self.space_id {
+                            if request_space_id != space_id {
+                                return Response::Error(ErrorResponse {
+                                    error: Error::NotFound,
+                                });
+                            }
+                            let user_in_space = in_space(client.get_user_id(), space_id.clone()).await;
+                            match user_in_space {
+                                Ok(user_in_space) => {
+                                    if !user_in_space {
+                                        return Response::Error(ErrorResponse {
+                                            error: Error::NotFound,
+                                        });
+                                    }
+                                    Response::GetChannel(GetChannelResponse { channel })
+                                },
+                                Err(error) => Response::Error(ErrorResponse { error })
+                            }
+                        } else {
+                            Response::Error(ErrorResponse {
+                                error: Error::NotFound,
+                            })
+                        }
+                    }
+                }
+
+
+            }
+            Err(error) => Response::Error(ErrorResponse { error }),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetChannelResponse {
+    channel: Channel,
+}
+
+// #[derive(Clone, Debug, Deserialize, Serialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct GetChannelsMethod {
+//     scope_id: Option<String>,
+// }
+// TODO: work out how scopes work with private channels
+
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CreateChannelMethod {
+    channel: ChannelInformation,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum ChannelInformation {
+    PrivateChannel {
+        target_id: String,
+        scope_id: Option<String>,
+    },
+    GroupChannel {
+        scope_id: String,
+        name: String,
+        description: Option<String>,
+    },
+    InformationChannel {
+        space_id: String,
+        scope_id: Option<String>,
+        name: String,
+        description: Option<String>,
+    },
+    AnnouncementChannel {
+        space_id: String,
+        scope_id: Option<String>,
+        name: String,
+        description: Option<String>,
+    },
+    ChatChannel {
+        space_id: String,
+        scope_id: Option<String>,
+        name: String,
+        description: Option<String>,
+    },
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EditChannelMethod {
+    channel_id: String,
+    name: Option<String>,
+    description: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+
+pub struct DeleteChannelMethod {
+    channel_id: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddUserToChannelMethod {
+    channel_id: String,
+    user_id: String,
+}
