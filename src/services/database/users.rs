9ed6daf026851c7782938e70e1ed9109e6f46530
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-01 14:35:54 -0500

    feat: friends

--- src/services/database/users.rs
+++ src/services/database/users.rs
@@ -16,17 +16,31 @@ pub struct Presence {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Relationship {
+    Friend = 0,
+    Blocked = 1,
+    Pending = 2,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Affinity {
+    id: String,
+    relationship: Relationship,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct User {
     id: String,
     profile_banner: String, // TODO: Make use of file handling
     profile_description: String,
     presence: Presence,
     online: bool,
-    display_name: String,
     // usernames on Nextflow are unique
     // can set a display name for better visibility
     platform_administrator: bool, // TODO: should be implemented globally
                                   // on SSO system user data
+    affinities: Vec<Affinity>,
 }
 
 pub async fn get_user(id: String) -> Result<Option<User>, Error> {
