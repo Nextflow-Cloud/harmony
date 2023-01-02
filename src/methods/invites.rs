f545ab5e9fc9c99e4cc3190c1195983401c1abc2
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-02 11:18:45 -0500

    refactor: restructure invite methods

--- src/methods/invites.rs
+++ src/methods/invites.rs
@@ -3,21 +3,18 @@ use dashmap::DashMap;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    errors::Error,
+    errors::{Error, Result},
     services::{
         database::{
-            channels::{get_channel, Channel},
             infractions::is_banned,
-            invites::{create_invite, delete_invite, get_invite, get_invites, Invite},
-            members::get_member,
-            spaces::get_space,
+            invites::{get_invites, Invite},
+            members::get_member, spaces::Space, channels::Channel,
         },
-        permissions::{permissions_for, Permission},
-        socket::RpcClient,
+        socket::RpcClient, permissions::Permission,
     },
 };
 
-use super::{ErrorResponse, Respond, Response};
+use super::{Respond, Response, authentication::check_authenticated};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CreateInviteMethod {
@@ -31,23 +28,19 @@ pub struct CreateInviteMethod {
 
 #[async_trait]
 impl Respond for CreateInviteMethod {
-    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Response {
-        let client = clients.get(&id).unwrap();
-        let user_id = client.get_user_id();
-        let invite = create_invite(
+    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Result<Response> {
+        let user = check_authenticated(&clients, &id)?;
+        let invite = Invite::create(
             self.channel_id.clone(),
-            user_id,
+            user.id.clone(),
             self.expires_at,
             self.max_uses,
             self.authorized_users.clone(),
             self.space_id.clone(),
             self.scope_id.clone(),
         )
-        .await;
-        match invite {
-            Ok(invite) => Response::CreateInvite(CreateInviteResponse { invite }),
-            Err(error) => Response::Error(ErrorResponse { error }),
-        }
+        .await?;
+        Ok(Response::CreateInvite(CreateInviteResponse { invite }))
     }
 }
 
@@ -89,28 +82,16 @@ pub struct DeleteInviteMethod {
 
 #[async_trait]
 impl Respond for DeleteInviteMethod {
-    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Response {
-        let client = clients.get(&id).unwrap();
-        let user_id = client.get_user_id();
-        let member = get_member(user_id, self.space_id.clone()).await;
-        match member {
-            Ok(member) => {
-                let permissions = permissions_for(member).await;
-                if !permissions.has_permission(Permission::ManageInvites) {
-                    return Response::Error(ErrorResponse {
-                        error: Error::MissingPermission {
-                            permission: Permission::ManageInvites,
-                        },
-                    });
-                } else {
-                    let result = delete_invite(self.id.clone()).await;
-                    match result {
-                        Ok(_) => Response::DeleteInvite(DeleteInviteResponse {}),
-                        Err(error) => Response::Error(ErrorResponse { error }),
-                    }
-                }
-            }
-            Err(error) => Response::Error(ErrorResponse { error }),
+    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Result<Response> {
+        let user = check_authenticated(&clients, &id)?;
+        let member = get_member(user.id.clone(), self.space_id.clone()).await?;
+        let permissions = member.get_permissions().await?;
+        if !permissions.has_permission(Permission::ManageInvites) {
+            return Err(Error::MissingPermission { permission: Permission::ManageInvites });
+        } else {
+            let invite = Invite::get(&self.id).await?;
+            invite.delete().await?;
+            Ok(Response::DeleteInvite(DeleteInviteResponse {}))
         }
     }
 }
@@ -127,71 +108,51 @@ pub struct GetInviteMethod {
 
 #[async_trait]
 impl Respond for GetInviteMethod {
-    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Response {
-        let client = clients.get(&id).unwrap();
-        let user_id = client.get_user_id();
-        let invite = get_invite(self.code.clone()).await;
-        match invite {
-            Ok(invite) => {
-                if let Some(space_id) = invite.space_id {
-                    let space = get_space(space_id).await;
-                    match space {
-                        Ok(space) => {
-                            let banned = is_banned(user_id.clone(), space.id).await;
-                            match banned {
-                                Ok(banned) => Response::GetInvite(GetInviteResponse {
-                                    invite: InviteInformation::Space {
-                                        name: space.name,
-                                        description: space.description,
-                                        inviter_id: invite.creator,
-                                        banned,
-                                        authorized: invite
-                                            .authorized_users
-                                            .unwrap_or(vec![user_id.clone()])
-                                            .contains(&user_id),
-                                        member_count: space.members.len() as i32,
-                                    },
-                                }),
-                                Err(error) => Response::Error(ErrorResponse { error }),
-                            }
-                        }
-                        Err(error) => Response::Error(ErrorResponse { error }),
-                    }
-                } else {
-                    let channel = get_channel(invite.channel_id).await;
-                    match channel {
-                        Ok(channel) => {
-                            if let Channel::GroupChannel {
-                                name,
-                                description,
-                                members,
-                                ..
-                            } = channel
-                            {
-                                Response::GetInvite(GetInviteResponse {
-                                    invite: InviteInformation::Group {
-                                        name,
-                                        description,
-                                        inviter_id: invite.creator,
-                                        authorized: invite
-                                            .authorized_users
-                                            .unwrap_or(vec![user_id.clone()])
-                                            .contains(&user_id),
-                                        member_count: members.len() as i32,
-                                    },
-                                })
-                            } else {
-                                Response::Error(ErrorResponse {
-                                    error: Error::InvalidInvite,
-                                })
-                            }
-                        }
-                        Err(error) => Response::Error(ErrorResponse { error }),
-                    }
-                }
+    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Result<Response> {
+        let user = check_authenticated(&clients, &id)?;
+        let invite = Invite::get(&self.code).await?;
+        if let Some(space_id) = invite.space_id {
+            let space = Space::get(&space_id).await?;
+            let banned = is_banned(user.id.clone(), space.id).await?;
+            Ok(Response::GetInvite(GetInviteResponse {
+                invite: InviteInformation::Space {
+                    name: space.name,
+                    description: space.description,
+                    inviter_id: invite.creator,
+                    banned,
+                    authorized: invite
+                        .authorized_users
+                        .unwrap_or(vec![user.id.clone()])
+                        .contains(&user.id),
+                    member_count: space.members.len() as i32,
+                },
+            }))
+        } else {
+            let channel = Channel::get(&invite.channel_id).await?;
+            if let Channel::GroupChannel {
+                name,
+                description,
+                members,
+                ..
+            } = channel
+            {
+                Ok(Response::GetInvite(GetInviteResponse {
+                    invite: InviteInformation::Group {
+                        name,
+                        description,
+                        inviter_id: invite.creator,
+                        authorized: invite
+                            .authorized_users
+                            .unwrap_or(vec![user.id.clone()])
+                            .contains(&user.id),
+                        member_count: members.len() as i32,
+                    },
+                }))
+            } else {
+                Err(Error::InvalidInvite)
             }
-            Err(error) => Response::Error(ErrorResponse { error }),
         }
+                
     }
 }
 
@@ -234,30 +195,17 @@ pub struct GetInvitesMethod {
 
 #[async_trait]
 impl Respond for GetInvitesMethod {
-    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Response {
-        let client = clients.get(&id).unwrap();
-        let user_id = client.get_user_id();
+    async fn respond(&self, clients: DashMap<String, RpcClient>, id: String) -> Result<Response> {
+        let user = check_authenticated(&clients, &id)?;
         if let Some(space_id) = &self.space_id {
-            let member = get_member(user_id, space_id.clone()).await;
-            match member {
-                Ok(member) => {
-                    let permissions = permissions_for(member).await;
-                    if !permissions.has_permission(Permission::ManageInvites) {
-                        return Response::Error(ErrorResponse {
-                            error: Error::MissingPermission {
-                                permission: Permission::ManageInvites,
-                            },
-                        });
-                    }
-                }
-                Err(error) => return Response::Error(ErrorResponse { error }),
-            };
+            let member = get_member(user.id.clone(), space_id.clone()).await?;
+            let permissions = member.get_permissions().await?;
+            if !permissions.has_permission(Permission::ManageInvites) {
+                return Err(Error::MissingPermission { permission: Permission::ManageInvites });
+            }
         }
-        let invites = get_invites(self.channel_id.clone(), self.space_id.clone()).await;
-        match invites {
-            Ok(invites) => Response::GetInvites(GetInvitesResponse { invites }),
-            Err(error) => Response::Error(ErrorResponse { error }),
-        }
+        let invites = get_invites(self.channel_id.clone(), self.space_id.clone()).await?;
+        Ok(Response::GetInvites(GetInvitesResponse { invites }))
     }
 }
 
