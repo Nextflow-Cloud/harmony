3a28ac9a1d7d14fd8a8736ed8b43ea55b3b29b3c
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-02 11:13:16 -0500

    refactor: restructure roles

--- src/services/database/roles.rs
+++ src/services/database/roles.rs
@@ -23,54 +23,84 @@ pub struct Color {
     pub blue: i32,
 }
 
-pub async fn create_role(
-    space_id: String,
-    name: String,
-    permissions: i64,
-    color: Color,
-) -> Result<Role> {
-    let roles = super::get_database().collection::<Role>("roles");
-    let space_roles = get_roles(space_id.clone()).await?;
-    let position = space_roles.len() as i32;
-    let role = Role {
-        id: Ulid::new().to_string(),
-        name,
-        permissions,
-        color,
-        position,
-        space_id,
-        scope_id: "global".to_string(), // FIXME: scope_id
-    };
-    roles.insert_one(role.clone(), None).await?;
-    Ok(role)
-}
+impl Role {
+    pub async fn create(
+        space_id: String,
+        name: String,
+        permissions: i64,
+        color: Color,
+    ) -> Result<Role> {
+        let roles = super::get_database().collection::<Role>("roles");
+        let space_roles = get_roles(space_id.clone()).await?;
+        let position = space_roles.len() as i32;
+        let role = Role {
+            id: Ulid::new().to_string(),
+            name,
+            permissions,
+            color,
+            position,
+            space_id,
+            scope_id: "global".to_string(), // FIXME: scope_id
+        };
+        roles.insert_one(role.clone(), None).await?;
+        Ok(role)
+    }
 
-pub async fn delete_role(role_id: String) -> Result<()> {
-    let roles = super::get_database().collection::<Role>("roles");
-    roles
-        .delete_one(
-            doc! {
-                "id": role_id,
-            },
-            None,
-        )
-        .await?;
-    Ok(())
-}
+    pub async fn delete(&self) -> Result<()> {
+        let roles = super::get_database().collection::<Role>("roles");
+        roles
+            .delete_one(
+                doc! {
+                    "id": &self.id,
+                },
+                None,
+            )
+            .await?;
+        Ok(())
+    }
 
-pub async fn get_role(role_id: String) -> Result<Role> {
-    let roles = super::get_database().collection::<Role>("roles");
-    let role = roles
-        .find_one(
-            doc! {
-                "id": role_id,
-            },
-            None,
-        )
-        .await?;
-    match role {
-        Some(role) => Ok(role),
-        None => Err(Error::NotFound),
+    pub async fn get(id: &String) -> Result<Role> {
+        let roles = super::get_database().collection::<Role>("roles");
+        let role = roles
+            .find_one(
+                doc! {
+                    "id": &id,
+                },
+                None,
+            )
+            .await?;
+        match role {
+            Some(role) => Ok(role),
+            None => Err(Error::NotFound),
+        }
+    }
+
+    pub async fn update(
+        &self,
+        name: String,
+        permissions: i64,
+        color: Color,
+    ) -> Result<Role> {
+        let roles = super::get_database().collection::<Role>("roles");
+        let role = roles
+            .find_one_and_update(
+                doc! {
+                    "id": &self.id,
+                },
+                doc! {
+                    "$set": {
+                        "name": name,
+                        "permissions": permissions,
+                        "color": bson::to_bson(&color)?,
+                    },
+                },
+                None,
+            )
+            .await?;
+        match role {
+            Some(role) => Ok(role),
+            None => Err(Error::NotFound),
+        }
     }
 }
 
@@ -85,32 +115,4 @@ pub async fn get_roles(space_id: String) -> Result<Vec<Role>> {
         )
         .await?;
     Ok(roles.try_collect().await?)
-}
-
-pub async fn update_role(
-    role_id: String,
-    name: String,
-    permissions: i64,
-    color: Color,
-) -> Result<Role> {
-    let roles = super::get_database().collection::<Role>("roles");
-    let role = roles
-        .find_one_and_update(
-            doc! {
-                "id": role_id,
-            },
-            doc! {
-                "$set": {
-                    "name": name,
-                    "permissions": permissions,
-                    "color": bson::to_bson(&color)?,
-                },
-            },
-            None,
-        )
-        .await?;
-    match role {
-        Some(role) => Ok(role),
-        None => Err(Error::NotFound),
-    }
 }
